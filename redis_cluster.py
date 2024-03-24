a624a3a557fa9634df5295b26e6701085d168929
Katya <77965300+KatyaProkhorchuk@users.noreply.github.com>
2024-03-24 21:25:19 +0300

    Create redis_cluster.py

---
+++ redis_cluster.py
@@ -0,0 +1,24 @@
+from rediscluster import RedisCluster
+
+startup_nodes = [
+    {"host": "172.17.0.2", "port": "6379"},
+    {"host": "172.17.0.2", "port": "6380"},
+    {"host": "172.17.0.2", "port": "6381"},
+]
+
+
+# Настройки таймаутов (в миллисекундах)
+socket_timeout = 3000
+socket_connect_timeout = 3000
+socket_keepalive = True 
+
+# Создаем кластер Redis с настройками таймаутов
+redis_cluster = RedisCluster(
+    startup_nodes=startup_nodes,
+    decode_responses=True,
+    socket_timeout=socket_timeout,
+    socket_connect_timeout=socket_connect_timeout,
+    socket_keepalive=socket_keepalive
+)
+
+print(redis_cluster.nodes.cluster_info())
